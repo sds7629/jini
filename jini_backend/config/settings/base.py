4f0d8d1dc3d89df5ba55d956552603c1e8a1adaf
sds7629 <sds7629@naver.com>
2023-09-27 13:45:02 +0900

    Unuse Session

--- jini_backend/config/settings/base.py
+++ jini_backend/config/settings/base.py
@@ -98,8 +98,8 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
