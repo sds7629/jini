5f708e8986ea78a6ceb97252c7c6770e751794ac
sds7629 <sds7629@naver.com>
2023-09-27 18:29:11 +0900

    social post

--- jini_backend/users/socialaccounts.py
+++ jini_backend/users/socialaccounts.py
@@ -133,7 +133,7 @@ KAKAO_CALLBACK_URI = "http://www.jinii.shop/api/v1/users/auth/kakao/callback"
     description="Social Login",
     summary="카카오 로그인",
 )
-@api_view(["GET"])
+@api_view(["POST"])
 def kakao_login(request):
     kakao_api = "https://kauth.kakao.com/oauth/authorize?response_type=code"
     redirect_uri = "http://www.jinii.shop/api/v1/users/auth/kakao/callback"
@@ -147,7 +147,7 @@ def kakao_login(request):
     description="Social Login",
     summary="카카오 로그인",
 )
-@api_view(["GET"])
+@api_view(["POST"])
 def kakao_callback(request):
     code = request.GET["code"]
 
