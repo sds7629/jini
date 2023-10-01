da0bb49d1edd1429e7260dd94e79de8a67a05ed2
sds7629 <sds7629@naver.com>
2023-10-01 20:41:35 +0900

    unactivate Token, Session authentication

--- jini_backend/config/settings/base.py
+++ jini_backend/config/settings/base.py
@@ -99,8 +99,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
