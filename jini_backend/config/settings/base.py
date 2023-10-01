1941666ef91e29bbb5085cdc4a6f4f2d235dc6b2
sds7629 <sds7629@naver.com>
2023-10-01 20:40:10 +0900

    email settings update

--- jini_backend/config/settings/base.py
+++ jini_backend/config/settings/base.py
@@ -99,8 +99,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        # "rest_framework.authentication.TokenAuthentication",
-        # "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
@@ -178,6 +178,39 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 LOGIN_REDIRECT_URL = "/"
 
+
+## Email Settings
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"  # 메일 호스트 서버
+
+EMAIL_PORT = "587"  # gmail과 통신하는 포트
+
+EMAIL_HOST_USER = "rinwoodev@gmail.com"  # 발신할 이메일
+
+EMAIL_HOST_PASSWORD = get_env_variable("EMAIL_HOST_PASSWORD")  # 발신할 메일의 비밀번호
+
+EMAIL_USE_TLS = True  # TLS 보안 방법
+
+SERVER_EMAIL = EMAIL_HOST_USER
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True  # 유저가 받은 링크를 클릭하면 회원가입 완료되게끔
+ACCOUNT_EMAIL_REQUIRED = True
+
+ACCOUNT_EMAIL_VERIFICATION = "jini"
+# ACCOUNT_EMAIL_VERIFICATION = "none"
+
+EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = (
+    "/"  # 사이트와 관련한 자동응답을 받을 이메일 주소,'webmaster@localhost'
+)
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+
+# 이메일에 자동으로 표시되는 사이트 정보
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "JINI"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
