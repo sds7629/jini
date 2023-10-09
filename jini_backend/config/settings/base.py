05b248f8dc1bfd9fb6499af289eda3f5d4d71bca
sds7629 <sds7629@naver.com>
2023-10-09 20:40:55 +0900

    gunicorn uvicorn and settings update

--- jini_backend/config/settings/base.py
+++ jini_backend/config/settings/base.py
@@ -227,7 +227,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static  files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
