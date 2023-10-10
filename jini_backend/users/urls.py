ed1b32708755b850c4edeb7c2870957072bdeeaf
sds7629 <sds7629@naver.com>
2023-10-10 14:01:22 +0900

    nginx up to react

--- jini_backend/users/urls.py
+++ jini_backend/users/urls.py
@@ -28,20 +28,14 @@ confirmpatterns = [
         "reset_passwd/",
         views.reset_password,
         name="reset_passwd",
-    )
-    # path(
-    #     "reset/<str:uid64>/<str:token>",views.reset_password_sendmail, name="reset"),
-    # ),
+    ),
 ]
 
 urlpatterns = [
     path("users/", include(confirmpatterns)),
     path("users/my_info/", views.get_info),
-    # path("users/auth/google/login/", socialaccounts.google_login),
     path("users/auth/google/callback", socialaccounts.google_callback),
-    # path("users/auth/kakao/login/", socialaccounts.kakao_login),
     path("users/auth/kakao/callback", socialaccounts.kakao_callback),
-    # path("users/auth/naver/login/", socialaccounts.naver_login),
     path("users/auth/naver/callback", socialaccounts.naver_callback),
     path("users/val_email/", views.validate_email),
     path("users/val_nickname/", views.validate_nickname),
@@ -49,5 +43,4 @@ urlpatterns = [
     path("users/Refresh/", TokenRefreshView.as_view()),
     path("users/register_user/", views.register_user),
     path("", include(router.urls)),
-    # path("users/<int:pk>/signout", views.signout),
 ]
