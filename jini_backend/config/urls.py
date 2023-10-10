53be98fd16f74d0cf869623be377d82e41ea2c75
sds7629 <sds7629@naver.com>
2023-10-10 11:22:53 +0900

    new admin urls

--- jini_backend/config/urls.py
+++ jini_backend/config/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/v1/", include("users.urls")),
     path("api/v1/", include("feeds.urls")),
     path("api/v1/", include("categories.urls")),
